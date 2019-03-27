1324508324d9b7a7de3186e8d1db4109fd9eb57c
ronsbons <ronnilouie@gmail.com>
2019-03-26 17:07:08 -0700

    continues work on styling and hamburger menu

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   console.log('js file loaded');
   console.log('jQuery working');
 
-})
+  $('.navbar-burger').on('click', function(event) {
+    event.preventDefault();
+    $('.navbar-burger').toggleClass('is-active');
+    $('.navbar-menu').toggleClass('is-active');
+  })
+});
