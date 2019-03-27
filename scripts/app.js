1b7ce5f3da2dd1e320867f326f245e079fa9e606
ronsbons <ronnilouie@gmail.com>
2019-03-26 21:33:13 -0700

    puts success message in a modal

--- scripts/app.js
+++ scripts/app.js
@@ -6,5 +6,16 @@ $(document).ready(function() {
     event.preventDefault();
     $('.navbar-burger').toggleClass('is-active');
     $('.navbar-menu').toggleClass('is-active');
-  })
+  });
+
+  // Testing modal open when form submits
+  $('#mc-embedded-subscribe-form').on('submit', function(event) {
+    $('.modal').toggleClass('is-active');
+  });
+
+  // Click function to close modal
+  $('.modal-close').on('click', function (event) {
+    event.preventDefault();
+    $('.modal').toggleClass('is-active');
+  });
 });
